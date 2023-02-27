7bd9fb50740f671876f598e0b21207645e6a2e99
Unknown <gamer@khimeraos>
2023-02-27 16:20:41 +0000

    Update build.sh and manifest

--- build.sh
+++ build.sh
@@ -108,6 +108,16 @@ pacman-key --populate
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen
 
+# chaotic-aur repos
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
+
+echo '
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+' >> /etc/pacman.conf
+
 # update package databases
 pacman --noconfirm -Syy
 
